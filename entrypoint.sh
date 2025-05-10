07f1912627f569672acb45f3cbdbae8aec8741ac
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-10 19:19:33 -0400

    refactor(entrypoint): add CI check to skip VPN steps and ensure container remains alive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 # ========== entrypoint.sh ==========
+set -euo pipefail
 
+# If in CI, skip VPN steps
+if [ "${CI:-false}" = "true" ]; then
+  echo "[Info] CI mode → skipping runtime"
+  exit 0
+fi
+
+# keep container alive
 exec tail -f /dev/null
