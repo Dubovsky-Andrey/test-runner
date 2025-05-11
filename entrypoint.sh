20a354edbe39c7340fe861e276d6cb776566a48e
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-10 23:18:45 -0400

    refactor(entrypoint): comment out NordVPN configuration commands for clarity

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,9 +18,9 @@ for i in $(seq 1 20); do
   sleep 2
 done
 
-nordvpn set killswitch on
-nordvpn set autoconnect on
-nordvpn whitelist add subnet 10.244.0.0/16
+#nordvpn set killswitch on
+#nordvpn set autoconnect on
+#nordvpn whitelist add subnet 10.244.0.0/16
 
 #sysctl -w net.ipv4.ip_forward=1
 #iptables -t nat -A POSTROUTING -o nordlynx -j MASQUERADE
