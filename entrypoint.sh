09fe5a2bcc069c8e0f0839993351a3a068e296b2
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-12 00:05:05 -0400

    refactor(entrypoint): replace timeout command with a for loop for socket readiness check

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,11 @@ dbus-daemon --system --fork
 nordvpnd &
 sleep 5
 
-timeout 60s bash -c '
-  until sockstat -n | grep -q "/run/nordvpn/nordvpnd.sock"; do
-    sleep 2
-    echo "[INFO] Waiting for nordvpnd.sock ready"
-  done
-'
+for i in $(seq 1 20); do
+  if [ -S /run/nordvpn/nordvpnd.sock ]; then
+    echo "nordvpnd socket ready"
+    break
+  fi
+  echo "waiting for nordvpnd.sock"
+  sleep 2
+done
