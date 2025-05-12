3b420f38ad27bff5b286895a9cb2e73a58c228c4
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-11 23:06:04 -0400

    refactor(entrypoint): replace socket readiness check loop with timeout command for improved clarity

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,5 @@ dbus-daemon --system --fork
 nordvpnd &
 sleep 5
 
-for i in $(seq 1 20); do
-  if [ -S /run/nordvpn/nordvpnd.sock ]; then
-    echo "nordvpnd socket ready"
-    break
-  fi
-  echo "waiting for nordvpnd.sock"
-  sleep 2
-done
-
-#nordvpn set killswitch on
-#nordvpn set autoconnect on
-#nordvpn whitelist add subnet 10.244.0.0/16
-
-#sysctl -w net.ipv4.ip_forward=1
-#iptables -t nat -A POSTROUTING -o nordlynx -j MASQUERADE
-
+timeout 120s bash -c 'while [ ! -S /run/nordvpn/nordvpnd.sock ]; do sleep 2; echo "[INFO] Waiting for nordvpnd.sock ready"; done'
+echo "[INFO] Nordvpnd socket ready
